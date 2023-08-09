65e0e1e3e12b471a6f11279491d80dc473c8883f
sergioRodriguez <sandres0190@gmail.com>
2023-08-09 13:13:51 -0500

    Se Aprendieron los elementos Text(), Column(), Row(), además el uso de las anotaciones @Composable y @Preview, por otra parte se usarón propiedades básicas en los textos y en el objeto Modifier

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -50,20 +50,27 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    //KOTLIN
+    implementation("androidx.core:core-ktx:1.10.1")
+    //EXTENSION FOR LIFECYCLESCOPE IN ACTIVITIES & FRAGMENTS
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    //ACTIVITY
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    //COMPOSE
+    implementation( platform("androidx.compose:compose-bom:2023.03.00") )
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    //UNIT TEST
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    //INSTRUMENTATION TEST
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
--- app/src/main/java/com/sergio/rodriguez/happybirthday/MainActivity.kt
+++ app/src/main/java/com/sergio/rodriguez/happybirthday/MainActivity.kt
@@ -3,13 +3,19 @@ package com.sergio.rodriguez.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sergio.rodriguez.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -23,29 +29,49 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GreetingText(message = "Happy Birthday Darlyn", from = "From: Sergio")
                 }
             }
         }
     }
 }
 
-@Preview(
-    showBackground = true,
-    )
+
+
 @Composable
-fun GreetingPreview() {
-    HappyBirthdayTheme {
-        Greeting("Android")
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
     }
 }
 
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    name = "My Preview"
+)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-        fontSize = 24.sp
-    )
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        GreetingText(message = "Happy Birthday Darlyn", from = "From Sergio")
+    }
 }
+
+
 
